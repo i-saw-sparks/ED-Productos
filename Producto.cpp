ae9c1f5e84acbb6099af03447557656062a705e2
i-saw-sparks <andresmolinaceti@gmail.com>
2020-09-25 19:33:57 -0500

    Abstraccion inicial de producto

--- Producto.cpp
+++ Producto.cpp
@@ -3,3 +3,67 @@
 //
 
 #include "Producto.h"
+
+const Fecha& Producto::getFechaEntrada() const {
+    return fechaEntrada;
+}
+
+void Producto::setFechaEntrada(const Fecha &fechaEntrada) {
+    Producto::fechaEntrada = fechaEntrada;
+}
+
+const std::array<char, 13> &Producto::getCodigoBarras() const {
+    return codigoBarras;
+}
+
+void Producto::setCodigoBarras(const std::array<char, 13> &codigoBarras) {
+    Producto::codigoBarras = codigoBarras;
+}
+
+const std::string &Producto::getNombre() const {
+    return nombre;
+}
+
+void Producto::setNombre(const std::string &nombre) {
+    Producto::nombre = nombre;
+}
+
+float Producto::getPeso() const {
+    return peso;
+}
+
+void Producto::setPeso(float peso) {
+    Producto::peso = peso;
+}
+
+float Producto::getPrecioMayoreo() const {
+    return precioMayoreo;
+}
+
+void Producto::setPrecioMayoreo(float precioMayoreo) {
+    Producto::precioMayoreo = precioMayoreo;
+}
+
+float Producto::getPrecioMenudeo() const {
+    return precioMenudeo;
+}
+
+void Producto::setPrecioMenudeo(float precioMenudeo) {
+    Producto::precioMenudeo = precioMenudeo;
+}
+
+int Producto::getExistencia() const {
+    return existencia;
+}
+
+void Producto::setExistencia(int existencia) {
+    Producto::existencia = existencia;
+}
+
+bool Producto::operator==(const Producto &rhs) const {
+    return codigoBarras == rhs.codigoBarras;
+}
+
+bool Producto::operator!=(const Producto &rhs) const {
+    return !(rhs == *this);
+}
--- Producto.h
+++ Producto.h
@@ -5,8 +5,42 @@
 #ifndef ACTIVIDAD2_PRODUCTO_H
 #define ACTIVIDAD2_PRODUCTO_H
 
+#include <array>
+#include <string>
+#include "Fecha.h"
 
 class Producto {
+private:
+    Fecha fechaEntrada;
+    std::array<char, 13> codigoBarras;
+    std::string nombre;
+    float peso, precioMayoreo, precioMenudeo;
+    int existencia;
+
+public:
+    const Fecha &getFechaEntrada() const;
+    void setFechaEntrada(const Fecha &fechaEntrada);
+
+    const std::array<char, 13> &getCodigoBarras() const;
+    void setCodigoBarras(const std::array<char, 13> &codigoBarras);
+
+    const std::string &getNombre() const;
+    void setNombre(const std::string &nombre);
+
+    float getPeso() const;
+    void setPeso(float peso);
+
+    float getPrecioMayoreo() const;
+    void setPrecioMayoreo(float precioMayoreo);
+
+    float getPrecioMenudeo() const;
+    void setPrecioMenudeo(float precioMenudeo);
+
+    int getExistencia() const;
+    void setExistencia(int existencia);
+
+    bool operator==(const Producto &rhs) const;
+    bool operator!=(const Producto &rhs) const;
 
 };
 
